9ae9285e8dcf1d138ffd0a7f445b4bcd12fc01cf
Surfboy678 <Surfboy678@users.noreply.github.com>
2019-01-25 20:10:11 +0100

    added spring security

--- src/main/java/com/spring/config/MainConfig.java
+++ src/main/java/com/spring/config/MainConfig.java
@@ -3,7 +3,9 @@ package com.spring.config;
 import com.spring.domain.repository.DBKnightRepository;
 import com.spring.domain.repository.InMemoryRepository;
 import com.spring.domain.repository.KnightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @Configuration
 public class MainConfig {
@@ -22,6 +24,14 @@ public class MainConfig {
         KnightRepository repo = new DBKnightRepository();
         return repo;
 
+
+    }
+    @Autowired
+    public void securityUsers(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("user1").roles("USER")
+                .and()
+                .withUser("user2").password("user2").roles("ADMIN");
 
     }
 }
